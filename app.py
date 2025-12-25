635494954d8c57f2fae800c73bf96d94f3ee630c
MaulidiahYasmin <maulidiahyasmin21@gmail.com>
2025-12-25 23:07:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 import streamlit as st
 import gspread
+import json
 from google.oauth2.service_account import Credentials
 
-st.title("Tes Library")
-st.success("gspread & google-auth berhasil di-load")
+st.title("Tes Koneksi Google Sheets")
+
+# Ambil credential dari Secrets
+creds_dict = json.loads(st.secrets["GOOGLE_CREDS_JSON"])
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+credentials = Credentials.from_service_account_info(
+    creds_dict,
+    scopes=scopes
+)
+
+client = gspread.authorize(credentials)
+
+# GANTI dengan NAMA spreadsheet kamu (harus sama persis)
+spreadsheet_name = "Recap Visit YOVI"
+sheet = client.open(spreadsheet_name).sheet1
+
+data = sheet.get_all_records()
+
+st.success("Berhasil terhubung ke Google Sheets 🎉")
+st.write(data)
+
